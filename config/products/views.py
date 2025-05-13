c9d6fe062a4bc56fd5092b341fa078a0b5b67cc1
aneenaprasad <aneenaaprasad@gmail.com>
2025-05-13 14:56:50 +0530

    product APIs

--- config/products/views.py
+++ config/products/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from rest_framework import generics, status, filters
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser, AllowAny
+# from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
-# Create your views here.
+# CATEGORY
+
+class CategoryCreateView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
+
+class CategoryUpdateView(generics.UpdateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = 'id'
+
+class CategoryDeleteView(generics.DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = 'id'
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+
+# PRODUCT
+
+class ProductCreateView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
+
+class ProductUpdateView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = 'id'
+
+class ProductDeleteView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = 'id'
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.filter(is_available=True)
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
+    lookup_field = 'id'
+
+class ProductSearchView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'category__name']
+    permission_classes = [AllowAny]
+
+class ProductFilterView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'price', 'stock']
+    permission_classes = [AllowAny]
+
+class ProductPaginatedListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = None  # Add custom pagination if needed
+    permission_classes = [AllowAny]
